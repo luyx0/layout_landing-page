60e264f0bed8dd374a10f37e30b2fd42c11cfa7f
Эluyx0 <arsen.romanenko.kb.2021@lpnu.ua>
2024-03-24 17:11:07 +0200

    make build

---
+++ index.ef778c6a.js
@@ -0,0 +1,2 @@
+window.addEventListener("scroll",function(){(window.pageYOffset||document.documentElement.scrollTop)>500?document.getElementById("back-to-top").style.display="block":document.getElementById("back-to-top").style.display="none"}),document.getElementById("back-to-top").addEventListener("click",function(){window.scrollTo({top:0,behavior:"smooth"})});
+//# sourceMappingURL=index.ef778c6a.js.map
